8f1572dcd9a78f81b4dc6b6117e6e1e2577b8456
nona12 <DAM@Equipo-701>
2017-11-13 14:51:32 +0100

    Modelo Valoracion Existencias

---
+++ src/vista/ModeloValoracionExistencias.java
@@ -0,0 +1,77 @@
+package vista;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import Controlador.Controlador;
+import Modelo.vo.Clientes;
+import Modelo.vo.LineasPedido;
+import Modelo.vo.Productos;
+import Modelo.vo.Proveedor;
+
+public class ModeloValoracionExistencias extends AbstractTableModel {
+	private static final Object String = null;
+	ArrayList<String> nombresColumnas;
+	ArrayList<Productos> datos;
+	
+	private String total="total";
+
+
+	public ModeloValoracionExistencias() {
+		nombresColumnas = new ArrayList<>();
+
+		nombresColumnas.add("Producto");
+		nombresColumnas.add("Stock");
+		nombresColumnas.add("Precio Compra");
+		nombresColumnas.add("Total");
+		
+
+		datos = new ArrayList<Productos>();
+		
+		// cargarExistencias();
+	}
+
+	public void cargarExistecias() {
+		Controlador controlador = new Controlador();
+		datos = controlador.cargarProductos();
+		
+	}
+
+	@Override
+	public int getColumnCount() {
+		return nombresColumnas.size();
+	}
+
+	@Override
+	public int getRowCount() {
+		return datos.size();
+	}
+
+	@Override
+	public Object getValueAt(int fila, int columna) {
+
+	
+		Productos productos = datos.get(fila);
+
+		switch (columna) {
+		case 0:
+			return productos.getProducto();
+		case 1:
+			return productos.getStock();
+		case 2:
+			return productos.getPrecioCompra();
+		case 3:
+			return total;
+		
+		}
+		return productos;
+	}
+
+	@Override
+	public String getColumnName(int columna) {
+		return nombresColumnas.get(columna);
+	}
+}
+
+
