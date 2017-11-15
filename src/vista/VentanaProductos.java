0c3511b0181b1df831b8752e462ab9ebe8d9854d
nuria hervella <nuriahervella@gmail.com>
2017-11-15 12:12:58 +0100

    Actualizacion ventana productos

--- src/vista/VentanaProductos.java
+++ src/vista/VentanaProductos.java
@@ -10,6 +10,8 @@ import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class VentanaProductos extends JDialog {
 
@@ -59,11 +61,44 @@ public class VentanaProductos extends JDialog {
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Mantenimiento Pedidos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(20, 11, 391, 91);
+		panel.setBounds(20, 36, 391, 80);
 		getContentPane().add(panel);
 		
+		JButton btnNuevo = new JButton("Nuevo");
+		panel.add(btnNuevo);
+		
+		JButton btnModificar = new JButton("Modificar");
+		panel.add(btnModificar);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		panel.add(btnEliminar);
+		
+		JButton btnConsultar = new JButton("Consultar");
+		panel.add(btnConsultar);
+		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(20, 113, 391, 91);
+		panel_1.setBorder(new TitledBorder(null, "Listado", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(20, 127, 391, 91);
 		getContentPane().add(panel_1);
+		
+		JButton btnListado = new JButton("Listado");
+		panel_1.add(btnListado);
+		
+		JButton btnMinimos = new JButton("Minimos");
+		panel_1.add(btnMinimos);
+		
+		JButton btnBeneficio = new JButton("Beneficio");
+		panel_1.add(btnBeneficio);
+		
+		JButton btnIncrementarPrecio = new JButton("Incrementar Precio");
+		panel_1.add(btnIncrementarPrecio);
+		
+		JButton btnValoracionExistencias = new JButton("Valoracion Existencias");
+		panel_1.add(btnValoracionExistencias);
+		
+		JLabel lblNewLabel = new JLabel("PRODUCTOS");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setBounds(137, 11, 114, 14);
+		getContentPane().add(lblNewLabel);
 	}
 }
